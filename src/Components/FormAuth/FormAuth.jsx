dd75a6633df8e9401310c92e129883a6a32ab90f
AndreyPervozvaniy <andreibelokudria@gmail.com>
2024-02-05 21:20:46 +0200

    add func to send form on mockAPI

--- src/Components/FormAuth/FormAuth.jsx
+++ src/Components/FormAuth/FormAuth.jsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Input, Text, Button, Flex } from "@chakra-ui/react";
 import { DeliverFormSchema } from "../../Utills/Schemas/DeliverySchema";
@@ -6,13 +6,15 @@ import { useToast } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import { clearCart } from "../../Redux/CartSlice";
 import { useDispatch } from "react-redux";
-
+import { useEffect } from "react";
 const FormDeliver = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
     reset,
+    setValue,
+    control,
   } = useForm({
     mode: "onChange",
     resolver: yupResolver(DeliverFormSchema),
@@ -20,31 +22,46 @@ const FormDeliver = () => {
   const dispatch = useDispatch();
   const toast = useToast();
 
-  const { totalPrice, totalCount } = useSelector((state) => state.CartSlice);
+  const { totalPrice, totalCount, cart } = useSelector(
+    (state) => state.CartSlice
+  );
+  useEffect(() => {
+    setValue("products", cart);
+  }, [cart, setValue]);
+  const onSubmit = async (data) => {
+    try {
+      const response = await fetch(
+        "https://65a93323219bfa371868c106.mockapi.io/Burger",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
 
-  const onSubmit = (data) => {
-    if (totalCount < 1) {
-      toast({
-        position: "top",
-        duration: 8000,
-        isClosable: true,
-        title: "Добавьте товар!",
-        description: `Ваша корзина пуста!`,
-        status: "error",
-      });
-    } else {
-      console.log(data);
-      dispatch(clearCart());
-      toast({
-        position: "top",
-        duration: 8000,
-        isClosable: true,
-        title: "Заказ принят!",
-        description: `Мы свяжимся с Вами в течении 5 минут по номеру  ${data.phone}! 
-       Сумма: ${totalPrice} грн.`,
-        status: "success",
-      });
-      reset();
+      if (response.ok) {
+        console.log("Форма успешно отправлена на сервер");
+        // Ваши дополнительные действия по успешной отправке
+        dispatch(clearCart());
+        toast({
+          position: "top",
+          duration: 8000,
+          isClosable: true,
+          title: "Заказ принят!",
+          description: `Мы свяжемся с Вами в течении 5 минут по номеру ${data.phone}! 
+            Сумма: ${totalPrice} грн.`,
+          status: "success",
+        });
+        reset();
+      } else {
+        console.error("Ошибка при отправке формы на сервер");
+        // Ваши действия в случае ошибки
+      }
+    } catch (error) {
+      console.error("Ошибка при отправке формы на сервер:", error);
+      // Ваши действия в случае ошибки
     }
   };
 
@@ -62,7 +79,24 @@ const FormDeliver = () => {
         <Text color={"red"}>{errors.phone?.message}</Text>
         <Text mt={5}>Почта*</Text>
         <Input {...register("email")} />
-        <Text color={"red"}>{errors.email?.message}</Text>
+        <Text color={"red"}>{errors.email?.message}</Text>{" "}
+        <Controller
+          name="products"
+          control={control}
+          defaultValue={[]} // Значение по умолчанию
+          render={({ field }) => (
+            <div>
+              {/* Отображение данных о продуктах в корзине */}
+              {field.value.map((product, index) => (
+                <div key={index}>
+                  <p>{product.name}</p>
+                  <p>{product.cost}</p>
+                  {/* Добавьте другие поля, если необходимо */}
+                </div>
+              ))}
+            </div>
+          )}
+        />
         <Button type="submit" mt={5}>
           <Text>Подтвердить</Text>
         </Button>
